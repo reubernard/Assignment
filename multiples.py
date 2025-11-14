211a563bc9a063b39845f4f5aa7bfdc3d5053b32
Bernard Reuben <bernardreuben1@gmail.com>
2025-11-14 07:21:04 +0100

    Add multiples checking for given numbers

---
+++ multiples.py
@@ -0,0 +1,13 @@
+#Multiples
+
+number = 1024
+if (number % 4 == 0):
+    print(f'{number} is a multiple of 4')
+else:
+    print(f'{number} is not a multiple of 4')
+
+number = 2
+if (number % 2 ==0):
+    print(f'{number} is a multiple of 10')
+else:
+    print(f'{number} is not a multiple of 10')
