450a3682c35806883568170cf14124b63c0a23f8
Bernard Reuben <bernardreuben1@gmail.com>
2025-11-14 10:25:58 +0100

    addition

--- age_calculator.py
+++ age_calculator.py
@@ -1,7 +1,9 @@
 #Prompt user to "Enter current father's age from 1 - 80:"
+
 #Read input of father_age
 
 #Prompt user to "Enter current son's age from 1 - 80:"
+
 #Read input of son_age
 
 #Calculate the years_difference = Absolute value of (father_age - 2 * son_age)
@@ -9,6 +11,7 @@
 #Display 'The father was twice as old as his son', years_difference, 'years ago'
 
 father_age = int(input("Enter current father's age from 1 - 80 : "))
+
 son_age = int(input("Enter current son's age from 1 - 80: "))
 
 years_difference = abs(father_age - 2 * son_age)
