a75b0b7e921c5a0b04b19f31366a1846c7da4b5c
Bernard Reuben <bernardreuben1@gmail.com>
2025-11-14 07:10:23 +0100

    Add circle parameters calculations

---
+++ circle_parameters.py
@@ -0,0 +1,9 @@
+#Circle Area, Diameter and Circumference
+
+radius = 2
+diameter = 2 * radius
+pie = 3.14159
+circumference = 2
+print('Diameter is', + diameter)
+print('Circumference is', + pie + radius)
+print('Area is', + pie + radius * radius)
