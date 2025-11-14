28fa46cae76bd92960b25c1e7fbb17df2177b968
Bernard Reuben <bernardreuben1@gmail.com>
2025-11-14 06:39:43 +0100

    Implement grade A congratulatory message
    Add code to print message for grade A.

---
+++ missing_code.py
@@ -0,0 +1,7 @@
+#Fill the missing code
+
+grade = 96
+if grade >= 90:
+    print(f"Congratulation, your grade of {grade} earns you an A in this course.")
+
+#f helps us to embed variables inside string. the variable must be put in curly braces
