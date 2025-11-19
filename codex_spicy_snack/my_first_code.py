64d27251794f7e18ca039ae04ad214aaece78cde
Bernard Reuben <bernardreuben1@gmail.com>
2025-11-19 03:58:31 +0100

    spicy snack

---
+++ codex_spicy_snack/my_first_code.py
@@ -0,0 +1,50 @@
+print("print Horizontally 1", end ='====')
+
+print("print Horizontally 2", end ='====')
+
+print("print Horizontally 3", end ='====')
+
+print("print Horizontally 4", end ='====')
+
+number = 34
+
+name = "Berny"
+
+amount = 90.56
+
+is_tall = True
+
+print('=' * 25)
+
+print(number**0.5)
+
+print("=" * 25)
+
+
+print("This is age's bike")
+print('This is age\'s bike')
+print("This hot tea is for Berny \\ Bolu")
+
+
+my_code_doc = """
+    The code is meant to collect user name and age 
+    and print it out one after the other
+
+"""
+
+print(my_code_doc)
+
+#Collecting input from user
+age = int(input("Enter user input: "))
+print(age, type(age))
+
+
+if age > 18:
+    print("you are eligible to vote in the forth coming election")
+
+elif age < 25
+
+
+else: print("You are not eligible to vote")
+
+
