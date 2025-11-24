92892f12f33c8069c640446f1faef90c524ddae0
Bernard Reuben <bernardreuben1@gmail.com>
2025-11-24 22:11:10 +0100

    spicy_snack

---
+++ codex_spicy_snack/error_code.py
@@ -0,0 +1,5 @@
+#What is wrong with this code?
+
+rating = input('Enter an integer rating between 1 and 10')
+
+#This code collects the rating value as a string. Therefore it needs to convert it to an integer. hence an error
