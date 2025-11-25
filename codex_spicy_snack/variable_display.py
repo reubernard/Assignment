7a8346baff93b78d3e5bf37212b9cb96c203982b
Bernard Reuben <bernardreuben1@gmail.com>
2025-11-25 08:28:22 +0100

    upload varriable in spicy

---
+++ codex_spicy_snack/variable_display.py
@@ -0,0 +1,10 @@
+#initialize the values of x and y
+#print the value of x in different form
+
+
+x = 2
+y = 3
+print('x =', x)
+print('Value of', x, '+', x, 'is', (x + x))
+print('x =')
+print((x + y), '=', (y + x))
